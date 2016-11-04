9e705da206af9a8fa8c3009c91929fdf3252e50d
yunjiang <yunjiang@pptv.com>
2016-11-04 18:09:00 +0800

    multi thread stack overflow error

--- src/main/java/com/jungle/jvm/VMError.java
+++ src/main/java/com/jungle/jvm/VMError.java
@@ -8,7 +8,8 @@ public class VMError {
 	public static void main(String[] args) {
 		//addObj();
 		try {
-			stackOverflow();
+			//stackOverflow();
+			multiThreadStackOverflow();
 		} catch (Throwable e) {
 			// TODO: handle exception			
 			System.out.println(stackDepth);
@@ -26,9 +27,51 @@ public class VMError {
 		}
 	}
 	
+	/**
+	 * VM args -Xss128k
+	 * 增加局部变量会增加每个方法调用的栈帧的size，所以溢出的时候，调用栈的深度减少。
+	 * The stack frame, also known as activation record is the collection of all data on the stack associated with one subprogram call.
+	 * 
+	 */
 	public static void stackOverflow(){
 		stackDepth++;
+	
+		long tmp=1;
+		long tmp1=2;
+		long tmp3=1;
+		long tmp4=2;
+		
+
 		stackOverflow();
 	}
+	
+	/**
+	 * VM args -Xss128k
+	 * 可能会死机
+	 */
+	public static void multiThreadStackOverflow() {
+		while (true) {
+			new Thread(new T1()).start();
+			stackDepth++;
+		}
+		
+	}
 
+}
+
+
+	class T1 implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			while (true) {
+	
+			}
+			
+		}
+		
+	
+	
+	
 }
