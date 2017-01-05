8219bd49f9039528b1600a30725a4d36beded783
yunjiang <yunjiang@pptv.com>
2017-01-05 18:46:19 +0800

    gc test

--- src/main/java/com/jungle/jvm/GCTest.java
+++ src/main/java/com/jungle/jvm/GCTest.java
@@ -1,5 +1,6 @@
 package com.jungle.jvm;
 
+import java.lang.ref.WeakReference;
 import java.util.Objects;
 
 /**
@@ -12,18 +13,26 @@ public class GCTest {
 
     /**
      * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:SurvivorRatio=8 -XX:+PrintGCDetails
+     * -Xloggc:gctest.log
+     * -XX:+HeapDumpOnOutOfMemoryError
      */
     public static void testAllocation(){
         final int bytes1MB = 1024*1024;
         byte[] b1 = new byte[2*bytes1MB];
         byte[] b2 = new byte[2*bytes1MB];
         byte[] b3 = new byte[2*bytes1MB];
+        WeakReference<Object> wo = new WeakReference<Object>(new Object());//live before next gc
 
-        byte[] b4 = new byte[2*bytes1MB];
+        System.out.println(wo.get().toString());
+        byte[] b4 = new byte[2*bytes1MB];//ygc
+        //System.out.println(wo.get().toString());//wo ref obj is collected by gc,Exception in thread "main" java.lang.NullPointerException
 
-        byte[] b5 = new byte[2*bytes1MB];
-        byte[] b6 = new byte[2*bytes1MB];
-        byte[] b7 = new byte[2*bytes1MB];
+        //byte[] b5 = new byte[2*bytes1MB];
+        //byte[] b6 = new byte[2*bytes1MB];
+        //byte[] b7 = new byte[2*bytes1MB];
+        //byte[] b8 = new byte[2*bytes1MB];
+        //byte[] b9 = new byte[2*bytes1MB];
+        //byte[] b10 = new byte[1*bytes1MB];
 
     }
 }
